44910660e5b4aa6a46e53080615608034adb0e90
Journey Park <tenderain@gmail.com>
2022-02-22 20:35:20 +0900

    update patch

--- src/controllers/NftController.ts
+++ src/controllers/NftController.ts
@@ -300,14 +300,14 @@ exports.getMetadata = async function (
   var tokenIds: any = req.query.tokenIds;
   tokenIds = tokenIds.split(",");
   var result = {};
-
+  var reload = false;
   try {
     var dbNftsObjs = await getRepository(NftItem).find({
       where: { tokenAddress: tokenAddr, tokenId: In(tokenIds) },
       relations: ["desc"],
     });
 
-    const nftInfos = dbNftsObjs.reduce(function (result: any, element) {
+    var nftInfos = dbNftsObjs.reduce(function (result: any, element) {
       if (result[element.tokenAddress]) {
         if (result[element.tokenAddress][element.tokenId]) {
           result[element.tokenAddress][element.tokenId] = element;
@@ -323,20 +323,32 @@ exports.getMetadata = async function (
     }, {});
 
     for (let i = 0; i < tokenIds.length; i++) {
-      if (!nftInfos[tokenAddr][tokenIds[i]]["desc"]) {
-        var [
-          name,
-          description,
-          image,
-          animationUrl,
-        ] = await nftService.getMetadata(tokenAddr, tokenIds[i]);
-        nftInfos[tokenAddr][tokenIds[i]]["desc"] = {
-          name: name,
-          description: description,
-          image: image,
-          animationUrl: animationUrl,
-        };
+      if (!(tokenAddr in nftInfos) || !(tokenIds[i] in nftInfos[tokenAddr])) {
+        await nftService.getMetadata(tokenAddr, tokenIds[i]);
+        reload = true;
       }
+    }
+
+    if (reload) {
+      dbNftsObjs = await getRepository(NftItem).find({
+        where: { tokenAddress: tokenAddr, tokenId: In(tokenIds) },
+        relations: ["desc"],
+      });
+
+      nftInfos = dbNftsObjs.reduce(function (result: any, element) {
+        if (result[element.tokenAddress]) {
+          if (result[element.tokenAddress][element.tokenId]) {
+            result[element.tokenAddress][element.tokenId] = element;
+          } else {
+            result[element.tokenAddress][element.tokenId] = element;
+          }
+        } else {
+          result[element.tokenAddress] = {};
+          result[element.tokenAddress][element.tokenId] = element;
+        }
+
+        return result;
+      }, {});
     }
 
     return res.status(200).json({ nfts: nftInfos });
